b6ce5764cfa3229e22d22a4d5eb75c5f53ae0735
= <christopher.dierker@gmail.com>
2017-04-08 23:52:50 -0700

    Added Basic Styling

--- todo.js
+++ todo.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($){
-   
+
     var model = {
         completed: [],
         incomplete: [],
@@ -12,11 +12,11 @@ jQuery(document).ready(function($){
         save: function(task) {
             model.incomplete.push({
                 id: model.incomplete.length,
-                task: task 
+                task: task
             });
         }
     };
-    
+
     var todo = {
         init: function() {
             nav.load();
@@ -27,7 +27,7 @@ jQuery(document).ready(function($){
         complete: function(task, id) {
             model.complete(task, id);
             var index = model.incomplete.findIndex(function(o) {
-                return o.id === id; 
+                return o.id === id;
             });
             model.incomplete.splice(index, 1);
         },
@@ -38,35 +38,35 @@ jQuery(document).ready(function($){
             return model.incomplete.length;
         },
         loadCompleted: function() {
-            complete.load(model.completed);   
+            complete.load(model.completed);
         },
         loadIncomplete: function() {
-            incomplete.load(model.incomplete);   
+            incomplete.load(model.incomplete);
         },
         removeTodo: function(id, list) {
             var index = model[list].findIndex(function(o) {
-                return o.id === id; 
+                return o.id === id;
             });
             model[list].splice(index, 1);
         }
     };
-    
+
     var nav = {
         load: function() {
-            var nav = "<nav><a class='incomplete' href='#'>Incomplete Tasks</a> <a class='complete' href='#'>Completed Tasks</a></nav>";   
+            var nav = "<nav><a class='incomplete' href='#'>Incomplete Tasks</a> <a class='complete' href='#'>Completed Tasks</a></nav>";
             nav += "<input type='text' class='new-task' placeholder='Input new task here...'/><br/>";
             nav += "<input type='button' class='add-task' value='+ Add'/><br/>";
             $("body").append(nav);
         }
     };
-    
+
     var app = {
         load: function() {
-            var app = "<div class='app'></div>";   
+            var app = "<div class='app'></div>";
             $("body").append(app);
         }
     };
-    
+
     var incomplete = {
         load: function(tasks) {
             var incomplete = "<div class='incomplete task-list'>";
@@ -87,6 +87,7 @@ jQuery(document).ready(function($){
             completed += "<h1>Completed Tasks</h1>";
             completed += "<ul class='task-list'>";
             $.each(tasks,function(i,x) {
+                completed += "<li><a href='#' class='remove'>X</a> <span id='"+tasks[i].id+"' class='task'>"+tasks[i].task+"</span></li>";
             });
             completed += "</ul>";
             completed += "</div>";
@@ -96,8 +97,11 @@ jQuery(document).ready(function($){
     var handlers = {
         load: function() {
             $(".add-task").on("click", function() {
-                var task = $(".new-task").val(); 
-                $(".incomplete.task-list").append("<li><a href='#' class='complete'>&#x2713</a> <a href='#' class='remove'>X</a> <span id='"+todo.count()+"' class='task'>"+task+"</span></li>");
+                var task = $(".new-task").val();
+                if (task == "" || task.length == 0) {
+                  return;
+                }
+                $(".incomplete.task-list > ul").append("<li><a href='#' class='complete'>&#x2713</a> <a href='#' class='remove'>X</a> <span id='"+todo.count()+"' class='task'>"+task+"</span></li>");
                 todo.save(task);
                 $(".new-task").val("");
             });
@@ -119,7 +123,7 @@ jQuery(document).ready(function($){
             });
         }
     };
-    
+
     todo.init();
-    
+
 });
